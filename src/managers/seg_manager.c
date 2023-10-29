02b8bbf055e8ae204b2c9532f59229adaba6ee9c
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-10-29 20:59:18 +0100

    basic of seg

--- src/managers/seg_manager.c
+++ src/managers/seg_manager.c
@@ -3,29 +3,78 @@
 #include "stdio.h"
 
 #include "../memory.h"
+
 #define MAX_PROGRAM_COUNT 20 // this value must be equal to the number of programs in test.c
 
 // Global Variables
-addr_t *base;
-size_t *heap;
-size_t *stack;
+addr_t *seg_base;
+size_t *seg_heap;
+size_t *seg_stack;
 
-int direction_size = -1;
+size_t *seg_bound_stack;
+size_t *seg_bound_seg_heap;
+
+int *seg_pids;
+int seg_valid_count = MAX_PROGRAM_COUNT;
+
+size_t *seg_virtual_mem; // -1 if is free, else pid
+
+int seg_current_pid_index = -1;
+int seg_bits = 1;
+int seg_direction_size = -1;
 
 // Utiles
-int binary()
+void binary_representation(int *repr, size_t value)
 {
-  
+  for (int i = 0; i < seg_direction_size; i++)
+  {
+    repr[i] = value % 2;
+    value /= 2;
+  }
+}
+size_t full_offset()
+{
+  return m_size();
 }
 
-int is_stack(addr_t addr) // 1 bit, 1 -> stack or 0 -> heap
+size_t offset(int *repr)
 {
-
+  size_t offsetv = 0;
+  int power = 1;
+  for (int i = 0; i < seg_direction_size - seg_bits; i++)
+  {
+    offsetv += power * i;
+    power *= 2;
+  }
+  return offsetv;
 }
 
-int get_addr(addr_t addr)
+int read_va(int *repr, size_t va) // return 1 if from stack
 {
+  binary_representation(repr, va);
+  return repr[seg_direction_size - seg_bits] == 1;
+}
 
+int get_pa(addr_t va_addr, addr_t *pa)
+{
+  int repr[seg_direction_size];
+  int is_stack = read_va(repr, va_addr);
+  size_t off = offset(repr);
+
+  if (is_stack && full_offset() - off >= seg_bound_stack[seg_current_pid_index])
+  {
+    printf("Out of bounds Offset: %ld  Bounds: %ld  IsStack: %d", off, seg_bound_stack[seg_current_pid_index], is_stack);
+    return 1;
+  }
+
+  if (!is_stack && off >= seg_bound_seg_heap[seg_current_pid_index])
+  {
+    printf("Out of bounds Offset: %ld  Bounds: %ld  IsStack: %d", off, seg_bound_seg_heap[seg_current_pid_index], is_stack);
+    return 1;
+  }
+
+  *pa = seg_base[seg_current_pid_index] + (is_stack) ? off - full_offset() : off;
+  return 0;
 }
 
 int how_many_bits_needed(size_t memory_size) // buscar si C tiene un log?
@@ -42,11 +91,13 @@ int how_many_bits_needed(size_t memory_size) // buscar si C tiene un log?
 // Esta función se llama cuando se inicializa un caso de prueba
 void m_seg_init(int argc, char **argv)
 {
-  int direction_bits = how_many_bits_needed(m_size());
-  printf("Tamanno de la memoria %ld, Se necesitan %d bits para direccionar toda la memoria\n", m_size(), direction_bits);
+  seg_direction_size = how_many_bits_needed(m_size());
+  printf("Tamanno de la memoria %ld, Se necesitan %d bits para direccionar toda la memoria\n", m_size(), seg_direction_size);
+  seg_direction_size += seg_bits;
+  printf("Agg el bit de seg: %d\n", seg_direction_size);
 }
 
-// Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
+// Reserva un espacio en el seg_heap de tamaño 'size' y establece un puntero al
 // inicio del espacio reservado.
 int m_seg_malloc(size_t size, ptr_t *out)
 {
