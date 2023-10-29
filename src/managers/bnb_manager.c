e5e978ff5f62249a455eda9f57f1d0517ba29c93
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-10-29 02:00:36 +0200

    ahi va

--- src/managers/bnb_manager.c
+++ src/managers/bnb_manager.c
@@ -4,9 +4,9 @@
 #include "../memory.h"
 #define MAX_PROGRAM_COUNT 20 // this value must be equal to the number of programs in test.c
 
-int current_pid = -1;
+int current_pid_index = -1;
 int *pids;
-size_t valid_count = 0;
+size_t valid_count = MAX_PROGRAM_COUNT;
 addr_t *bases;
 size_t bound;
 
@@ -15,7 +15,7 @@ size_t *stack_pointer;
 
 int **virtual_mem; // 1 if is reserved
 
-#define SIZEOFVALUES 4
+#define SIZEOFVALUES 1
 
 int find_pid(int pid)
 {
@@ -25,23 +25,31 @@ int find_pid(int pid)
   return -1;
 }
 
+int get_free_pid()
+{
+  for (size_t i = 0; i < valid_count; i++)
+    if (pids[i] == -1)
+      return i;
+  return -1;
+}
+
 int add_pid(int pid)
 {
   int index = find_pid(pid);
+  int free = get_free_pid();
 
-  if (index >= 0)
+  if (index >= 0 || free < 0)
     return 1;
 
-  pids[valid_count] = pid;
-  valid_count += 1;
+  pids[free] = pid;
   return 0;
 }
 
 int sim_stack_op(int is_del)
 {
-  size_t stack_pointer_temp = stack_pointer[current_pid] + (is_del ? SIZEOFVALUES : -SIZEOFVALUES);
+  size_t stack_pointer_temp = stack_pointer[current_pid_index] + (is_del ? SIZEOFVALUES : -SIZEOFVALUES);
 
-  if (stack_pointer_temp > bound || stack_pointer_temp < heap_pointer[current_pid])
+  if (stack_pointer_temp > bound || stack_pointer_temp < heap_pointer[current_pid_index])
     return 1;
   return 0;
 }
@@ -54,61 +62,78 @@ int valid_stack_op(int is_del)
 
 int in_addr(addr_t addr)
 {
-  if (bases[current_pid] > addr || addr > bases[current_pid] + bound)
+  if (bases[current_pid_index] > addr || addr > bases[current_pid_index] + bound)
     return 1;
   return 0;
 }
 
 int find_addr(addr_t addr)
 {
-  if (in_addr(addr) || addr > stack_pointer[current_pid])
+  if (in_addr(addr) || addr > stack_pointer[current_pid_index])
     return -1;
-  return addr - bases[current_pid];
+  return addr - bases[current_pid_index];
 }
 
 int delete_addr(addr_t addr)
 {
   int index = find_addr(addr);
-  if (index < 0 || virtual_mem[current_pid][index] == 0)
+  if (index < 0 || virtual_mem[current_pid_index][index] == 0)
     return 1;
 
-  virtual_mem[current_pid][index] = 0;
+  virtual_mem[current_pid_index][index] = 0;
   return 0;
 }
 
-int valid_malloc_heap_op(size_t size)
+size_t min(size_t x, size_t y)
+{
+  return x < y ? x : y;
+}
+
+int find_malloc_size(size_t size, size_t *slots)
 {
   size_t spaces = size / SIZEOFVALUES;
-  if (heap_pointer[current_pid] + spaces > bound || heap_pointer[current_pid] + spaces > stack_pointer[current_pid])
-    return 1;
-  return 0;
+  int count = 0;
+  for (size_t i = 0; i < min(stack_pointer[current_pid_index], bound); i++)
+  {
+    if (count == spaces)
+      return 0;
+
+    if (virtual_mem[current_pid_index][i] == 0)
+    {
+      slots[count] = i;
+      count++;
+    }
+
+    else
+      count = 0;
+  }
+  return 1;
 }
 
 int update_heap_pointer() // TODO: check this function
 {
-  for (size_t i = 0; i < stack_pointer[current_pid]; i++)
-    if (virtual_mem[current_pid][i])
-      heap_pointer[current_pid] = i;
+  for (size_t i = 0; i < stack_pointer[current_pid_index]; i++)
+    if (virtual_mem[current_pid_index][i])
+      heap_pointer[current_pid_index] = i;
   return 0;
 }
 
-int resb_addr(addr_t addr)
+int resb_addr(size_t index_addr)
 {
-  if (find_addr(addr) < 0 || virtual_mem[current_pid][find_addr(addr)] || valid_malloc_heap_op(SIZEOFVALUES))
+  if (in_addr(bases[current_pid_index] + index_addr) || virtual_mem[current_pid_index][index_addr])
     return 1;
-  virtual_mem[current_pid][stack_pointer[current_pid]] = 1;
+  virtual_mem[current_pid_index][index_addr] = 1;
   update_heap_pointer();
-  return 0;
 }
 
 // Esta función se llama cuando se inicializa un caso de prueba
 void m_bnb_init(int argc, char **argv)
 {
   // initialize global variables
-  current_pid = -1;
+  current_pid_index = -1;
 
   pids = (int *)malloc(MAX_PROGRAM_COUNT * sizeof(int));
-  valid_count = 0;
+  valid_count = MAX_PROGRAM_COUNT;
 
   bases = (addr_t *)malloc(MAX_PROGRAM_COUNT * sizeof(addr_t));
   bound = 0;
@@ -119,6 +144,10 @@ void m_bnb_init(int argc, char **argv)
   virtual_mem = (int **)malloc(MAX_PROGRAM_COUNT * sizeof(int *));
   for (int i = 0; i < MAX_PROGRAM_COUNT; i++)
     virtual_mem[i] = (int *)malloc(bound * sizeof(int));
+
+  // No valid pids
+  for (int i = 0; i < MAX_PROGRAM_COUNT; i++)
+    pids[i] = -1;
 
   // Reset to 0
   for (int i = 0; i < MAX_PROGRAM_COUNT; i++)
@@ -140,8 +169,9 @@ void m_bnb_init(int argc, char **argv)
 // inicio del espacio reservado.
 int m_bnb_malloc(size_t size, ptr_t *out)
 {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+  size_t slot[size];
+  if (find_malloc_size(size, slot))
+    return 1;
 }
 
 // Libera un espacio de memoria dado un puntero.
@@ -182,8 +212,7 @@ int m_bnb_store(addr_t addr, byte val)
 // Notifica un cambio de contexto al proceso 'next_pid'
 void m_bnb_on_ctx_switch(process_t process)
 {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+  current_pid_index = find_pid(process.pid);
 }
 
 // Notifica que un proceso ya terminó su ejecución
