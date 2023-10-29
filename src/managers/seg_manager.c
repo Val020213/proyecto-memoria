59b509d2768c7500289e82208f49e95ab6cdab33
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-10-29 22:50:08 +0100

    candela

--- src/managers/seg_manager.c
+++ src/managers/seg_manager.c
@@ -14,10 +14,14 @@ size_t *seg_stack;
 size_t *seg_bound_stack;
 size_t *seg_bound_seg_heap;
 
+size_t seg_default_stack_bound;
+// size_t seg_default_heap_bound;
+
 int *seg_pids;
 int seg_valid_count = MAX_PROGRAM_COUNT;
 
-size_t *seg_virtual_mem; // -1 if is free, else pid
+int *seg_virtual_mem; // -1 if is free, else pid
+size_t seg_virtual_mem_size;
 
 int seg_current_pid_index = -1;
 int seg_bits = 1;
@@ -86,6 +90,133 @@ int how_many_bits_needed(size_t memory_size) // buscar si C tiene un log?
     bits++;
   }
   return bits;
+}
+
+int seg_find_pid(int pid)
+{
+  for (int i = 0; i < MAX_PROGRAM_COUNT; i++)
+    if (seg_pids[i] == pid)
+      return i;
+  return -1;
+}
+
+int find_free_pid()
+{
+  for (int i = 0; i < MAX_PROGRAM_COUNT; i++)
+    if (seg_pids[i] == -1)
+      return i;
+  return -1;
+}
+
+int seg_add_pid(int pid)
+{
+  int index = seg_find_pid(pid);
+  int free = find_free_pid();
+
+  if (index >= 0 || free < 0)
+  {
+    printf("Error: pid %d already exists or there is no free space\n", pid);
+    return 1;
+  }
+
+  seg_pids[free] = pid;
+  return 0;
+}
+
+int seg_del_pid(int pid)
+{
+  int index = seg_find_pid(pid);
+
+  if (index < 0)
+  {
+    printf("Error: pid %d does not exists\n", pid);
+    return 1;
+  }
+
+  seg_pids[index] = -1;
+  return 0;
+}
+
+size_t seg_min(size_t x, size_t y)
+{
+  return x < y ? x : y;
+}
+
+// free list
+
+void free_v_memo(int l, int r)
+{
+  for (int i = l; i < r; i++)
+  {
+    seg_virtual_mem[i] = -1;
+  }
+  m_unset_owner(l, r - 1);
+}
+
+int first_fit(size_t need)
+{
+  for (size_t i = 0; i < seg_virtual_mem_size; i++)
+    if (seg_virtual_mem[i] == -1)
+    {
+      size_t j = i;
+      while (j < seg_virtual_mem_size && seg_virtual_mem[j] == -1)
+        j++;
+      if (j - i >= need)
+        return i;
+      i = j;
+    }
+  return -1;
+}
+
+void fusion_fit()
+{
+  for (int i = 0; i < seg_valid_count; i++)
+  {
+    if (seg_pids[i] >= 0)
+    {
+      if (seg_stack[i] < seg_bound_stack[i])
+        free_v_memo(seg_stack[i], seg_bound_stack[i] + 1);
+      if (seg_heap[i] < seg_bound_seg_heap[i])
+        free_v_memo(seg_heap[i], seg_bound_seg_heap[i] + 1);
+    }
+  }
+}
+
+int extend_proc(int need_stack, int need_heap)
+{
+  int addr_stack = seg_base[seg_current_pid_index] - seg_bound_stack[seg_current_pid_index];
+
+  while (need_stack > 0)
+  {
+    if (seg_virtual_mem[addr_stack] != seg_pids[seg_current_pid_index] && seg_virtual_mem[addr_stack] != -1)
+    {
+      printf("Error: Stack is not free\n");
+      return 1;
+    }
+    free_v_memo(addr_stack, addr_stack - 1);
+    addr_stack--;
+    need_stack--;
+  }
+  m_set_owner(addr_stack, seg_base[seg_current_pid_index] - seg_bound_stack[seg_current_pid_index]);
+  seg_bound_stack[seg_current_pid_index] += need_stack;
+
+  int addr_heap = seg_base[seg_current_pid_index] + seg_bound_seg_heap[seg_current_pid_index];
+
+  while (need_heap > 0)
+  {
+    if (seg_virtual_mem[addr_heap] != seg_pids[seg_current_pid_index] && seg_virtual_mem[addr_heap] != -1)
+    {
+      printf("Error: Heap is not free\n");
+      return 1;
+    }
+    free_v_memo(addr_heap, addr_heap + 1);
+    addr_heap++;
+    need_heap--;
+  }
+
+  m_set_owner(seg_base[seg_current_pid_index] + seg_bound_seg_heap[seg_current_pid_index], addr_heap);
+  seg_bound_seg_heap[seg_current_pid_index] += need_heap;
+  return 0;
 }
 
 // Esta función se llama cuando se inicializa un caso de prueba
